e58ecf4d4e82d377cf64c1d5c431bc8186462688
Felipe B Silva <programming1129@gmail.com>
2021-09-30 20:59:09 -0300

    Adicionando twitter, e começando a fazer o menu lateral

---
+++ twitter-clone/src/components/constante/LinksMenu.js
@@ -0,0 +1,52 @@
+const LINKS = [
+    {
+        nome: 'Pagina Inicial',
+        alt: 'Pagina Inicial',
+        href: '#',
+        cName: 'link link--paginaInicial'
+    },    
+    {
+        nome: 'Explorar',
+        alt: 'Explorar',
+        href: '#',
+        cName: 'link link--explorar'
+    },
+    {
+        nome: 'Notificações',
+        alt: 'Notificações',
+        href: '#',
+        cName: 'link link--notificacoes'
+    },
+    {
+        nome: 'Mensagens',
+        alt: 'Mensagens',
+        href: '#',
+        cName: 'link link--mensagens'
+    },
+    {
+        nome: 'Itens salvos',
+        alt: 'Itens salvos',
+        href: '#',
+        cName: 'link link--itensSalvos'
+    },
+    {
+        nome: 'Listas',
+        alt: 'Listas',
+        href: '#',
+        cName: 'link link--listas'
+    },
+    {
+        nome: 'Perfil',
+        alt: 'Perfil',
+        href: '#',
+        cName: 'link link--perfil'
+    },
+    {
+        nome: 'Mais',
+        alt: 'Mais',
+        href: '#',
+        cName: 'link link--mais'
+    }
+]
+
+export default LINKS
